1f53db7e6db864f42600070adea727b2a56613ff
WhyTheMoon <xzhu9839@usc.edu>
2023-02-27 13:41:51 -0800

    Update newmodel.py

--- newmodel.py
+++ newmodel.py
@@ -219,24 +219,149 @@ class myGPT(pl.LightningModule):
                 now_token_out = torch.unsqueeze(self.drop(canvas[i] + in_pe),0) # [1, t, D]
                 seq_len = len(canvas[i])
                 out = self.decoder_blocks(now_token_out, self.mask[:seq_len, :seq_len]) # [1, t, D]
-                now_logit = self.head(out)
-                logits.append(now_logit)
+                now_logit = torch.squeeze(self.head(out)) # [t, Vc]
+                logits.append(now_logit) # # (!) understand what does this shifter mean!
         else:
             logits = self.head(net.reshape(b,t,-1))
 
         return logits
 
-    # def training_step(self, batch, batch_idx, eval=False):
-    #     if self.config_num_prefix == 0:
-    #         x, y = batch
-    #         logits = self(x)
-    #     else:
-    #         x, y, x_mask, y_mask = batch
-    #         logits = self(x, x_mask)  # change forward to adapt to input mask
+    def training_step(self, batch, batch_idx):
+        if self.config.num_prefix == 0:
+            x, y = batch
+            logits = self(x)
+        else:
+            x, y, x_mask, y_mask = batch #(?) are masks here 2 dimensional boolean mask? are the masks at word/subword level?
+            logits = self(x, x_mask) # [B, t, Vc] (!) add adaption to input masks
         
-    #     loss = None
-    #     if y is not None:
-    #         if self.config.num_prefix > 0:
-    #             loss = F.cross_entropy() # change cross_entropy to adapt to different dimensionality
-                    
-    
+        loss = None
+        if y is not None: # y [B, t, Vc]
+            if self.config.num_prefix > 0:
+                loss = []
+                b, _, _=logits.size()
+                for i in range(b):
+                    loss.append(F.cross_entropy(logits[i], y[i]))
+            else:
+                loss = F.cross_entropy(logits.transpose(1,2), y)
+        
+        if not eval:
+            self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("gpu", torch.cuda.memory_allocated() / (1024 ** 3), on_epoch=True, prog_bar=True, logger=True)
+        return loss
+        
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
+        for _ in range(max_new_tokens):
+            idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            logits = self(idx_cond)
+            logits = logits[:][-1][:]/temperature # get the last logits (the prediction)
+            b, t, Vc = logits.size()
+            probs = []
+            if top_k is not None:
+                v = []
+                for i in range(b):
+                    v, _ = torch.topk(logits[i], top_k) # v = [B, 1, top_k]
+                    logits[i][logits[i] < v[:, [-1]]] = -float('Inf')
+            
+            for i in range(b):
+                probs.append(F.softmax(logits[i], dim=-1))
+            
+            if do_sample:
+                idx_next = torch.multinomidal(probs, num_samples=1) # (!) change to sequential
+            else:
+                _, idx_next = torch.topk(probs, k=1, dim=-1)
+            idx = torch.cat((idx, idx_next), dim=1) 
+        return idx
+            
+    def validation_step(self, batch, batch_idx):
+        logging_batch_idx = -1 
+        #self.log('val_loss', self.training_step(batch, batch_idx, eval=True), on_step=False, on_epoch=True, logger=True)
+        context=90
+        max_new_tokens=30
+        if self.config.base == 'char':
+            space_token = 0
+        elif self.config.base == 'byte':
+            space_token = 35
+            #context=30
+            max_new_tokens=60
+        if self.config.num_prefix == 0 and self.config.base != 'word':
+            x,y = batch
+            b,l = x.shape
+            
+            inputs = torch.zeros((b,context), dtype=x.dtype, device=x.device)
+            answers = torch.zeros((b,max_new_tokens), dtype=x.dtype, device=x.device)
+            lens = torch.zeros(b, dtype=x.dtype, device=x.device)
+            for i,row in enumerate(x):
+                if self.config.base in ['char','byte']:
+                    spaces = (row==space_token).nonzero()
+                    last_word_beg, last_word_end = spaces[-2][0], spaces[-1][0]+1 # last full word
+                    inputs[i] = row[last_word_beg - context+1:last_word_beg+1] # +1 to include space
+                    answers[i,:last_word_end-last_word_beg-1] = row[last_word_beg+1:last_word_end]
+                    lens[i] = last_word_end - last_word_beg -1 # 2. if gt is bank, banker should be wrong.
+                elif self.config.base == 'sub':
+                    spaces = (row.unsqueeze(1)==self.news.to(row.device)).any(dim=1).nonzero() # b size boolean, then nonzero
+                    last_word_beg, last_word_end = spaces[-2][0], spaces[-1][0] # last full word
+                    inputs[i] = row[last_word_beg - context:last_word_beg]
+                    answers[i,:last_word_end-last_word_beg] = row[last_word_beg:last_word_end]
+                    lens[i] = last_word_end - last_word_beg
+            out = self.generate(inputs, max_new_tokens=max_new_tokens, temperature=1.0, do_sample=True, top_k=None)
+            out = out[:,context:]
+            mask = torch.arange(max_new_tokens, device=x.device).expand(len(lens), max_new_tokens) < lens.unsqueeze(1)
+            corrects = (out==answers)*mask # if gt is bank, banker should be wrong.
+            acc_unit = corrects.sum()/mask.sum() # sub/char
+            acc_word = (corrects.float() + (1- mask.float())).bool().all(dim=-1).float().mean() # TODO: sub: penalize prefix match
+            if batch_idx == logging_batch_idx:
+                if self.config.base == 'char':
+                    rows = [["".join([self.rev[_c] for _c in _context]).strip(), "".join([self.rev[_c] for _c in _pred]).strip(), "".join([self.rev[_c] for _c in _true]).strip()] for _context, _pred, _true in zip(inputs.cpu().tolist(), out.cpu().tolist(), answers.cpu().tolist()) ]
+                elif self.config.base in ['sub','byte']:
+                    rows = [[self.config.vocab.decode(_context), self.config.vocab.decode([_c for _c in _pred if _c!=0]), self.config.vocab.decode([_c for _c in _true if _c!=0]).strip() ] for _context, _pred, _true in zip(inputs.cpu().tolist(), out.cpu().tolist(), answers.cpu().tolist() ) ]
+        else:
+             # b,c0 # note: (x[:,1]==y[:,0]).all()
+            if self.config.base == 'word':
+                x,y = batch
+                b,l = x.shape
+                logits = self(x[:,-context:])
+                preds = torch.argmax(logits[:,-1],dim=-1) # b
+                true = y[:,-1]
+                if batch_idx == logging_batch_idx:
+                    rows = [[" ".join([self.rev[_c] for _c in _context]), self.rev[_pred], self.rev[_true].strip()] for _context, _pred, _true in zip(x[:,-context:].cpu().tolist(), preds.cpu().tolist(), true.cpu().tolist())]
+            else: # e2e
+                x, y, x_mask, y_mask = batch
+                b,l,c = x.shape
+                #logits = self(x[:,-context:], x_mask[:, -context:]) # b,context-1,c0,V
+                #intermediates = []
+                preds = torch.zeros((b,1),device=x.device,dtype=x.dtype)
+                for i in range(c): # TODO: efficiently store output for the last word, just run last AR layer
+                    logits = self(x[:,-context:], x_mask[:, -context:]) # b,context-1,c0,V
+                    logits_ = logits[:, -1, i, :] / 1.0 # b,V
+                    probs = F.softmax(logits_, dim=-1)
+                    idx_next = torch.multinomial(probs, num_samples=1)
+                    preds = torch.cat((preds, idx_next),dim=1)
+                    #idx = torch.cat((idx, idx_next), dim=1)
+                    # check if idx_next matches original x[:,-1,i] for unit_acc. or keep aggregating, will retain old preds
+                    x[:,-1,i] = idx_next.squeeze() # x[:,-1] is only used to initialize canvas with hints.
+                    #intermediates.append(idx_next.squeeze().cpu().tolist())
+                    #'''
+                #preds = torch.stack(intermediates, dim=1) # b,c0
+                #preds = torch.argmax(logits[:,-1],dim=-1) # b,c0
+                preds = preds[:,1:]
+                true = y[:,-2]
+                if batch_idx == 0:
+                    if self.config.base == 'char':
+                        rows = [[" ".join(["".join([self.rev[_c] for _c in _word]).strip() for _word in _context]), "".join([self.rev[_c] for _c in _pred]).strip(), "".join([self.rev[_c] for _c in _true]).strip()] for _context, _pred, _true in zip(y[:,-(context+2):-2].cpu().tolist(), preds.cpu().tolist(), true.cpu().tolist())]
+                    elif self.config.base in ['sub','byte']:
+                        rows = [[" ".join([self.config.vocab.decode([_c for _c in _word if _c != 0]) for _word in _context]), self.config.vocab.decode([_c for _c in _pred if _c != 0]), self.config.vocab.decode([_c for _c in _true if _c != 0]).strip()] for _context, _pred, _true in zip(y[:,-(context+2):-2].cpu().tolist(), preds.cpu().tolist(), true.cpu().tolist())]
+            corrects = (preds==true)
+            acc_unit = (corrects*(true!=0).float()).sum() / (true!=0).float().sum() # sub/char TODO: exclude 0s
+            if self.config.base == 'word':
+                acc_word = acc_unit
+            else:
+                acc_word = corrects.all(dim=-1).float().mean()
+        if batch_idx == logging_batch_idx:
+            headers = ["context", "pred", "true"]
+            #rows.insert(0, headers)
+            self.logger.experiment.log_table("predictions.csv", [[f"\"{cell}\"" for cell in row] for row in rows], headers=headers)
+        #rev = {k:v for v,k in self.config.vocab.items()}
+        self.log("gpu", torch.cuda.memory_allocated() / (1024 ** 3), on_epoch=True, prog_bar=True, logger=True)
+        self.log('acc_unit', acc_unit, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log('acc_word', acc_word, on_step=True, on_epoch=True, prog_bar=True, logger=True)
